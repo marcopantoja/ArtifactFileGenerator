0e53bb9d0714ac1ea6a4455af9daf8d67bb09f78
Marco Pantoja <marco.pantoja1@hp.com>
2021-12-15 00:03:12 -0800

    Initial script for converting cmm data to Artifact files for n-Sphere ball plates.
    updated instructions in README.md file
    
    Upload initial data for tracking purposes

---
+++ csv-files-to-artifact-files.py
@@ -0,0 +1,157 @@
+from genericpath import isfile
+from os import walk,getcwd,listdir,makedirs
+from os.path import join,isdir,basename
+from csv import DictWriter
+from sys import argv
+from numpy import std,mean
+from datetime import datetime as dt
+
+
+# check args
+if len(argv)>0:
+    if '--help' in argv:
+        print('''
+        This script is meant for finding csv file(s) contained in a folder
+        with cmmx file(s). The measurements are averaged across runs to 
+        make one artifact file per directory. The artifact will be given a name
+        that matches the directory name where data is stored.
+        
+        Output:
+        The artifact files will be stored in the cmmx folder for each artifact,
+        unless a common folder is specified at runtime. 
+        
+        Options:
+        
+        --folder    narrow search for files by providing a path to
+                    a folder
+        --output    path to directory where all artifact files should 
+                    be saved to.''')
+    if '--folder' in argv:
+        cwd = argv[argv.index('--folder')+1]
+        if not isdir(cwd):raise Exception('invalid folder argument!')
+    else:
+        cwd = getcwd()
+    if '--output' in argv:
+        outputDir = argv[argv.index('--output')+1]
+    else:
+        outputDir = None
+
+def flatten_cmm_csv(filepath:str):
+    flat = {'file':filepath}
+    with open(filepath, 'r') as csvfile:
+        data = [r.strip('ï»¿\n').replace('Â','').split(',') for r in csvfile]
+    after_notes = False
+    for item in data:
+        if item[0] == 'Notes':
+            after_notes = True
+            continue
+        if not after_notes and item[1] != '' and item[0] not in flat: 
+            if item[0]=='Temperature':
+                flat[f'{item[0]}_{item[1][-2:]}'] = item[1][:-2]
+            else:
+                flat[item[0]] = item[1]
+        elif after_notes:
+            m_type = item[2]
+            measures = {'nominal':3,'actual':6,'error':9}
+            for val_type in measures:
+                if m_type == 'Position Cartesian':
+                        for id,direc in enumerate(['x','y','z']):
+                            flat[f'{item[1]}_{val_type}_{direc}'] = item[measures[val_type]+id]
+                elif m_type == 'Diameter':
+                    flat[f'{item[2]}_{item[1]}_{val_type}'] = item[measures[val_type]]
+                elif m_type == 'PointPointDistance':
+                    flat[f'{item[2]}_{item[1]}_{val_type}'] = item[measures[val_type]]
+    return flat
+
+def output_compiled_csv(data:list,filepath:str):
+    fields = {}
+    for d in data:
+        for i in d:
+            try: fields[i] += 1
+            except KeyError: fields[i] = 1
+    with open(filepath,'w',newline='') as csvout:
+        writer = DictWriter(csvout,data[0],extrasaction='raise')
+        writer.writeheader()
+        writer.writerows(data)
+    return True
+
+def average_data(data:list):
+    averages = {}
+    for data in all_data:
+        for d in data:
+            if d.startswith('Pos ') and 'actual' in d:
+                label = f'{d[4:6]}_{d[-1]}'
+                try: averages[label].append(float(data[d]))
+                except KeyError: averages[label] = [float(data[d])]
+            elif d.startswith('Diameter') and 'actual' in d:
+                label = 'S'+d.split('_')[1][1:]
+                try: averages[label].append(float(data[d]))
+                except KeyError: averages[label] = [float(data[d])]
+            elif d.startswith('PointPointDistance') and 'actual' in d:
+                label = d.split('_')[1]
+                try: averages[f'{label[0:2]}-{label[-1]}'].append(float(data[d]))
+                except KeyError: averages[f'{label[0:2]}-{label[-1]}'] = [float(data[d])]
+            elif 'Temperature' in d:
+                try: averages[d[:-3]+'degC'].append(float(data[d]))
+                except KeyError: averages[d[:-3]+'degC'] = [float(data[d])]
+    return {a:(mean(averages[a]),std(averages[a]),len(averages[a])) for a in averages if len(averages[a])}
+
+for r,d,f in walk(cwd):
+    d = [i.lower() for i in d]
+    cmmx = [i.lower() for i in f if i.endswith('.cmmx')]
+    if 'reports' in d and len(cmmx):
+        artifact = basename(r)
+        reports = join(r,'reports')
+        csvs = [join(reports,f) for f in listdir(reports) if f.endswith('.csv')]
+        artifact_summary = join(reports,artifact+'_summary.csv')
+        if not len(csvs): continue# no csv files found
+        elif isfile(artifact_summary): continue
+        print('Compiling: ',r)
+        all_data = []
+        for csv in csvs:
+            all_data.append(flatten_cmm_csv(csv))
+        output_compiled_csv(all_data, artifact_summary)
+        # all_data averaged
+        averages = average_data(all_data)
+        # make artifact file
+        artifact_name = artifact+f'_SDME-Avg_{dt.now().strftime("%y%m%d")}.artifact'
+        if outputDir is not None and isdir(outputDir):
+            artifact_path = join(outputDir,artifact_name)
+        elif outputDir is not None:
+            try:
+                makedirs(outputDir)
+                artifact_path = join(outputDir,artifact_name)
+            except:pass
+        else:
+            artifact_path = join(r,artifact_name)
+        
+        spheres = len([int(s[1:]) for s in averages if '_' not in s and '-' not in s and s[1:].isnumeric()])
+        try:
+            inspector = all_data[0]['Inspector']
+        except:
+            inspector = ''
+        with open(artifact_path,'w') as artifactFile:
+            sphere_info = ''
+            for i in range(spheres):
+                sphere_info+=f'''
+            <sphere name="S{i}" x="{averages[f"S{i}_x"][0]}" y="{averages[f"S{i}_y"][0]}" z="{averages[f"S{i}_z"][0]}" diameter="{averages[f"S{i}"][0]}" count="{averages[f"S{i}"][2]}" stdev_mm="{averages[f"S{i}"][1]}" CTE_m_m_K="8.6E-06"/>'''
+            distance_info = ''
+            for dist in averages:
+                if dist.startswith('L') and '-' in dist:
+                    distance_info+=f'''
+            <sphereCenterDistance name="{dist}" sphereA="S{dist[1]}" sphereB="S{dist[-1]}" distance="{averages[dist][0]}" count="{averages[dist][2]}" stdev_mm="{averages[dist][1]}" CTE_m_m_K="1.2E-06"/>'''
+            contents = f'''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<artifact type="{artifact_name}" name="{artifact_name} SDME Avg" date="{dt.now().strftime('%Y-%m-%d')}" revision="0">
+	<changelog>
+		<revision value="0" date="{dt.now().strftime('%Y-%m-%d')}" description="Initial python generated file, from {len(csvs)} csv cmm measurement files." />
+	</changelog>
+	<alignmentGuide icr="None" rotX="0" rotY="0" rotZ="0" angleTolerance="60" />
+	<scanGuide dynamicRange="DRP2" />
+	<pointFilter maxNormalAngleError="20" outlierRemoval="0.3" />
+	<metrologyInfo date="{all_data[0]['Inspection Time'][:4]}-{all_data[0]['Inspection Time'][4:6]}-{all_data[0]['Inspection Time'][6:8]}" lab="SDME" tool="Axiom" metrologist="{inspector}" temperatureDegC="{averages['TemperaturedegC'][0]}" comment="31 touch pts, 10 deg below equator; Average of South and East Orientations; n={averages['TemperaturedegC'][-1]} (2 orientations x 6 reps/orientation x 3 sessions); Ball Plate coord system defined by Datum A: best fit plane through spheres, Datum B: line through sphere centers 1 and 2 projected onto Datum A, Origin at S0. Ball Plate placed directly on CMM table. Assumed Titanium Ti-6Al-4V spheres."/>
+	<spheres>{sphere_info}
+	</spheres>
+	<distanceMeasures>{distance_info}
+	</distanceMeasures>
+</artifact>'''
+            artifactFile.write(contents)
